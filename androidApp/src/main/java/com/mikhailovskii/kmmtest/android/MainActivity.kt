daedda5148b59953a382e6f083ffd93b7c13a82a
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-06-24 00:16:55 +0300

    Added TextFields

--- androidApp/src/main/java/com/mikhailovskii/kmmtest/android/MainActivity.kt
+++ androidApp/src/main/java/com/mikhailovskii/kmmtest/android/MainActivity.kt
@@ -3,14 +3,51 @@ package com.mikhailovskii.kmmtest.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello world")
+            LoginWindow()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun LoginWindow() {
+        var login by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+        Box(Modifier.fillMaxSize()) {
+            Spacer(
+                Modifier
+                    .matchParentSize()
+                    .background(Color(1f, 1f, 1f))
+            )
+            Column(Modifier.width(IntrinsicSize.Max)) {
+                OutlinedTextField(
+                    value = login,
+                    onValueChange = { login = it },
+                    label = { Text("Login") }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") }
+                )
+            }
         }
     }
 }
