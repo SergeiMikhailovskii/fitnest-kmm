b1f42ae7c49d1038230a02bc188243f89474439d
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-07-06 00:41:31 +0300

    [IOS] Added hiding keyboard by click at empty area

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -26,6 +26,8 @@ struct ContentView: View {
                     })
                 }
             }
+        }.onTapGesture {
+            hideKeyboard()
         }
     }
 }
@@ -35,3 +37,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
