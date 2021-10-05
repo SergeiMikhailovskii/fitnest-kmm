b89a9d484ce228fc26d90f37389760f1287367e6
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-10-06 00:57:49 +0300

    [Android] Add base for onboarding

--- androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/onboarding/OnboardingScreen.kt
+++ androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/onboarding/OnboardingScreen.kt
@@ -1,12 +1,23 @@
 package com.mikhailovskii.kmmtest.android.view.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.FabPosition
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+/**
+ * This import should not be deleted because app will crash without it
+ */
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.mikhailovskii.kmmtest.android.R
 import com.mikhailovskii.kmmtest.entity.OnboardingState
@@ -22,10 +33,25 @@ fun OnboardingScreen(navController: NavController, type: String) = withDI {
         viewModel.updateScreenState(type)
     }
 
-    Scaffold {
-        Image(
-            painterResource(id = screenState?.imageResId ?: R.drawable.ic_onboarding_first),
-            contentDescription = null
-        )
+    Scaffold(floatingActionButton = {
+        Button(onClick = {}, modifier = Modifier.size(50.dp),shape = CircleShape) {
+            Text("A")
+        }
+    }, floatingActionButtonPosition = FabPosition.End) {
+        Column {
+            Image(
+                painter = painterResource(
+                    id = screenState?.imageResId ?: R.drawable.ic_onboarding_first
+                ),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(text = "Track Your Goal", modifier = Modifier.padding(top = 64.dp, start = 30.dp))
+            Text(
+                text = "Don't worry if you have trouble determining your goals, We can help you determine your goals and track your goals",
+                modifier = Modifier.padding(top = 15.dp, start = 30.dp, end = 30.dp)
+            )
+        }
     }
 }
