f557a223f7d8d2fbcbc70ac420e14f7c827462a7
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-10-08 01:00:45 +0300

    [Android] Moved button title to resources

--- androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/splash/SplashScreen.kt
+++ androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/splash/SplashScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,7 +53,7 @@ fun SplashScreen(navController: NavController) {
                     .align(Alignment.BottomCenter),
             ) {
                 Text(
-                    text = "Get Started",
+                    text = stringResource(id = R.string.splash_button_title),
                     fontFamily = poppinsFamily,
                     fontWeight = FontWeight.Bold,
                     fontSize = 16.sp
