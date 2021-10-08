d9a2e2a5928ccaed2977ca445f81a4444d976b90
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-10-09 00:21:42 +0300

    [Android] Add accomponist animation

--- androidApp/src/main/java/com/mikhailovskii/kmmtest/android/base/FitnestApp.kt
+++ androidApp/src/main/java/com/mikhailovskii/kmmtest/android/base/FitnestApp.kt
@@ -1,24 +1,28 @@
 package com.mikhailovskii.kmmtest.android.base
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
-import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.mikhailovskii.kmmtest.android.style.FitnestTheme
 import com.mikhailovskii.kmmtest.android.view.login.LoginScreen
 import com.mikhailovskii.kmmtest.android.view.onboarding.OnboardingScreen
 import com.mikhailovskii.kmmtest.android.view.splash.SplashScreen
 
+@ExperimentalAnimationApi
 @Composable
 fun FitnestApp() {
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
 
     FitnestTheme {
         Scaffold {
-            NavHost(navController = navController, startDestination = Route.Splash.screenName) {
+            AnimatedNavHost(navController = navController, startDestination = Route.Splash.screenName) {
                 composable(route = Route.Splash.screenName) {
                     SplashScreen(navController = navController)
                 }
@@ -27,9 +31,15 @@ fun FitnestApp() {
                 }
                 composable(
                     route = "onboarding/{type}",
-                    arguments = listOf(navArgument("type") { type = NavType.StringType })
+                    arguments = listOf(navArgument("type") { type = NavType.StringType }),
+                    enterTransition = { _, _ ->
+                        fadeIn(animationSpec = tween(2000))
+                    }
                 ) {
-                    OnboardingScreen(navController = navController, type = it.arguments?.getString("type") ?: "")
+                    OnboardingScreen(
+                        navController = navController,
+                        type = it.arguments?.getString("type") ?: ""
+                    )
                 }
             }
         }
