484b4e7973b97d5f900d2b9a2360dc548546bd3b
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-10-10 12:59:14 +0300

    [Android] Add progress to FAB

--- androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/ui_elements/GradientButton.kt
+++ androidApp/src/main/java/com/mikhailovskii/kmmtest/android/view/ui_elements/GradientButton.kt
@@ -1,17 +1,39 @@
 package com.mikhailovskii.kmmtest.android.view.ui_elements
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.mikhailovskii.kmmtest.android.R
+import com.mikhailovskii.kmmtest.android.style.BrandColor
+import com.mikhailovskii.kmmtest.android.style.BrandGradient
+
+@Preview
+@Composable
+fun GradientButtonPreview() {
+    GradientButton(
+        gradient = Brush.horizontalGradient(BrandGradient),
+        size = 50.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_onboarding_arrow_right),
+            contentDescription = null
+        )
+    }
+}
 
 @Composable
 fun GradientButton(
@@ -33,21 +55,34 @@ fun GradientButton(
                 this
             }
         }
-    Button(
-        modifier = buttonModifier,
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
-        contentPadding = PaddingValues(),
-        onClick = { onClick() },
-        shape = shape
+    Box(
+        modifier = Modifier
+            .width(IntrinsicSize.Min)
+            .height(IntrinsicSize.Min)
     ) {
-        Box(
+        CircularProgressIndicator(
+            modifier = Modifier.fillMaxSize(),
+            progress = 0.25F,
+            strokeWidth = 2.dp,
+            color = BrandColor
+        )
+        Button(
             modifier = Modifier
-                .background(gradient)
-                .fillMaxSize()
-                .then(modifier),
-            contentAlignment = Alignment.Center,
+                .padding(10.dp)
+                .then(buttonModifier),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            contentPadding = PaddingValues(),
+            onClick = { onClick() },
+            shape = shape
         ) {
-            body()
+            Box(
+                modifier = Modifier
+                    .background(brush = gradient)
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                body()
+            }
         }
     }
 }
