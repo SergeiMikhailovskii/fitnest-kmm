8cecb3acbbd16859ad5bca096f7b4cc3c48abcb8
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-10-21 00:57:20 +0300

    [Android] Created CookieType

--- shared/src/androidMain/kotlin/com/mikhailovskii/kmmtest/cookie/CookiesStorageImpl.kt
+++ shared/src/androidMain/kotlin/com/mikhailovskii/kmmtest/cookie/CookiesStorageImpl.kt
@@ -1,26 +1,34 @@
 package com.mikhailovskii.kmmtest.cookie
 
 import android.content.Context
+import com.mikhailovskii.kmmtest.enum.CookieType
 import io.ktor.http.*
 import org.kodein.di.DI
 import org.kodein.di.instance
 
 actual class CookiesStorageImpl actual constructor(actual val di: DI) {
 
-    private val cookieMap = mutableMapOf<String, String>()
-
     private val context: Context by di.instance()
+
+    private val preferences by lazy {
+        context.getSharedPreferences("fitnestapp", Context.MODE_PRIVATE)
+    }
 
     actual fun addCookie(requestUrl: Url, cookie: Cookie) {
         println("Set cookie name=${cookie.name}, value=${cookie.value}")
-        cookieMap[cookie.name] = cookie.value
-        context.getSharedPreferences("kmm_preferences", Context.MODE_PRIVATE)
-        println()
+        val editor = preferences.edit()
+        if (CookieType.values().any { it.value == cookie.name }) {
+            editor.putString(cookie.name, cookie.value)
+        }
+        editor.apply()
     }
 
     actual fun getCookies() = mutableListOf<Cookie>().apply {
-        cookieMap.forEach {
-            this.add(Cookie(it.key, it.value))
+        CookieType.values().forEach { cookie ->
+            preferences.getString(cookie.value, null)?.let {
+                println("Loaded: ${cookie.value}=${it}")
+                add(Cookie(cookie.value, it))
+            }
         }
     }
 
---
+++ shared/src/commonMain/kotlin/com/mikhailovskii/kmmtest/enum/CookieType.kt
@@ -0,0 +1,6 @@
+package com.mikhailovskii.kmmtest.enum
+
+enum class CookieType(val value: String) {
+    AUTH_USER("AUTH_USER"),
+    TEST("test")
+}
