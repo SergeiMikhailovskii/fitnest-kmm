295e6f3952a30c9e9b8cd9f6df498b0f8f8c6c5a
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-11-07 19:25:24 +0300

    [Android] Removed deprecated items

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,11 +11,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(31)
+    compileSdk = 31
     defaultConfig {
         applicationId = "com.mikhailovskii.kmmtest.android"
-        minSdkVersion(21)
-        targetSdkVersion(31)
+        minSdk = 21
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
     }
@@ -29,10 +29,8 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        useIR = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.0.0-beta09"
-        kotlinCompilerVersion = "1.5.10"
     }
 }
