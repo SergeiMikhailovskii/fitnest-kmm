f78543e97509060614b25411347c9c1bf54ebed3
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-11-25 01:32:21 +0300

    [Android] Column width to max

--- androidApp/src/main/kotlin/com/fitnest/android/view/registration/RegistrationScreen.kt
+++ androidApp/src/main/kotlin/com/fitnest/android/view/registration/RegistrationScreen.kt
@@ -1,14 +1,20 @@
 package com.fitnest.android.view.registration
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun RegistrationScreen(navController: NavController, stepName: String) {
     Scaffold {
-        Column {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text("Hey there,")
             Text("Create an Account")
             TextField(value = "", onValueChange = { })
