584aee57dd4588448ac59b05f804d7a50981accc
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2022-09-03 23:14:12 +0300

    [Android] Added other platforms for domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,13 +1,36 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
+    kotlin("native.cocoapods")
     kotlin("plugin.serialization") version "1.5.30"
+    id("com.android.library")
 }
 
 version = "1.0"
 
 kotlin {
-    jvm()
-    ios()
+    android()
+
+    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+            ::iosArm64
+        else
+            ::iosX64
+
+    iosTarget("ios") {}
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        ios.deploymentTarget = "14.1"
+        frameworkName = "domain"
+
+//        framework {
+//            baseName = "domain"
+//        }
+    }
+    
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -18,5 +41,23 @@ kotlin {
                 api(KodeinDependencies.Common.KODEIN_COMMON)
             }
         }
+        val commonTest by getting
+        val androidMain by getting
+        val androidTest by getting
+        val iosMain by getting {
+            dependsOn(commonMain)
+        }
+        val iosTest by getting {
+            dependsOn(commonTest)
+        }
+    }
+}
+
+android {
+    compileSdk = 32
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
     }
 }
