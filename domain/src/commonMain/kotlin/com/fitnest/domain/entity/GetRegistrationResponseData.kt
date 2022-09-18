d0d6506fe541048c1a4f81a6715aaab03e088d6b
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2022-09-18 17:43:36 +0300

    Formatting

--- domain/src/commonMain/kotlin/com/fitnest/domain/entity/GetRegistrationResponseData.kt
+++ domain/src/commonMain/kotlin/com/fitnest/domain/entity/GetRegistrationResponseData.kt
@@ -1,7 +1,5 @@
 package com.fitnest.domain.entity
 
-import com.fitnest.domain.entity.validator.Validator
-
 data class GetRegistrationResponseData(
     val step: String? = null,
     val fields: RegistrationStepModel? = null,
