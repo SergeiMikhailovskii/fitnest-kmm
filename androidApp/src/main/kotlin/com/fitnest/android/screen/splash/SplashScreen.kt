f324f48f3a10d1d125d4967924a85ecf2bdc1aeb
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2022-10-02 22:40:07 +0300

    Revert "[Android] Commit with other signing"
    This reverts commit 9bf86cae8b65d50f74e54e12e8dbeebabad7da21.

--- androidApp/src/main/kotlin/com/fitnest/android/screen/splash/SplashScreen.kt
+++ androidApp/src/main/kotlin/com/fitnest/android/screen/splash/SplashScreen.kt
@@ -38,7 +38,6 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.kodein.di.compose.rememberInstance
 
-//test
 @Composable
 fun SplashScreen(navController: NavController) {
     val viewModelFactory: ViewModelProvider.Factory by rememberInstance()
