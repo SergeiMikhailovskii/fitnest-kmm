789073ea3b88f8b2d029893105ed1c9d8afb8c18
Sergei Mikhailovskii <33265881+SergeiMikhailovskii@users.noreply.github.com>
2022-11-19 00:41:14 +0300

    [Android] Added napier (#67)

--- data/src/commonMain/kotlin/com/fitnest/service/NetworkService.kt
+++ data/src/commonMain/kotlin/com/fitnest/service/NetworkService.kt
@@ -4,11 +4,13 @@ import com.fitnest.cookie.CookiesStorage
 import com.fitnest.domain.entity.base.BaseResponse
 import com.fitnest.domain.service.NetworkService
 import com.fitnest.network.Endpoints
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.post
@@ -31,6 +33,11 @@ class NetworkService(private val cookiesStorage: CookiesStorage) : NetworkServic
             })
         }
         install(Logging) {
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Napier.i(message)
+                }
+            }
             level = LogLevel.ALL
         }
     }
