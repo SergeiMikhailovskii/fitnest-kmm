f08c0d4c631ca323bb79b2240210d31b99f88b2a
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2022-11-27 19:47:19 +0300

    [Android] Refactored top bar

--- androidApp/src/main/kotlin/com/fitnest/android/base/TopBar.kt
+++ androidApp/src/main/kotlin/com/fitnest/android/base/TopBar.kt
@@ -2,6 +2,7 @@ package com.fitnest.android.base
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.fitnest.android.R
+import com.fitnest.android.enum.TopBarType
 import com.fitnest.android.style.Dimen
 import com.fitnest.android.style.Padding
 
@@ -26,43 +28,76 @@ fun TopBar(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route.orEmpty()
 
-    if (isRouteWithTopBar(currentRoute)) {
-        Row(
-            Modifier
-                .height(Dimen.Dimen48)
-                .padding(horizontal = Padding.Padding16),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_top_bar_back),
-                contentDescription = null,
-                modifier = Modifier.clickable(onClick = navController::navigateUp)
-            )
-            Text(
-                topBarScreenName(currentRoute),
-                modifier = Modifier.weight(1F),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    fontWeight = FontWeight.Bold
-                )
-            )
-            Image(
-                painter = painterResource(id = R.drawable.ic_top_bar_options),
-                contentDescription = null
-            )
-        }
+    when (getTopBarType(currentRoute)) {
+        TopBarType.WITH_BACK_TITLE_AND_OPTIONS -> TopBarWithBackTitleAndOptions(
+            navController = navController,
+            currentRoute = currentRoute
+        )
+        TopBarType.ONLY_TITLE -> TopBarWithTitle(
+            currentRoute = currentRoute
+        )
+        TopBarType.NONE -> Box {}
     }
-
 }
 
-private fun isRouteWithTopBar(route: String) = arrayOf(
+@Composable
+private fun TopBarWithBackTitleAndOptions(navController: NavController, currentRoute: String) {
+    Row(
+        Modifier
+            .height(Dimen.Dimen48)
+            .padding(horizontal = Padding.Padding16),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_top_bar_back),
+            contentDescription = null,
+            modifier = Modifier.clickable(onClick = navController::navigateUp)
+        )
+        Text(
+            topBarScreenName(currentRoute),
+            modifier = Modifier.weight(1F),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_top_bar_options),
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+private fun TopBarWithTitle(currentRoute: String) {
+    Row(
+        Modifier
+            .height(Dimen.Dimen48)
+            .padding(horizontal = Padding.Padding16),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            topBarScreenName(currentRoute),
+            modifier = Modifier.weight(1F),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
+
+private fun getTopBarType(route: String) = when (route) {
     Route.PrivateAreaNotifications.screenName,
-    Route.PrivateAreaActivityTracker.screenName
-).contains(route)
+    Route.PrivateAreaActivityTracker.screenName -> TopBarType.WITH_BACK_TITLE_AND_OPTIONS
+    Route.PrivateAreaSettings.screenName -> TopBarType.ONLY_TITLE
+    else -> TopBarType.NONE
+}
 
 @Composable
 private fun topBarScreenName(route: String) = when (route) {
     Route.PrivateAreaNotifications.screenName -> stringResource(id = R.string.private_area_notifications_screen_title)
     Route.PrivateAreaActivityTracker.screenName -> stringResource(id = R.string.private_area_activity_tracker_screen_title)
+    Route.PrivateAreaSettings.screenName -> stringResource(id = R.string.private_area_profile_screen_title)
     else -> ""
 }
