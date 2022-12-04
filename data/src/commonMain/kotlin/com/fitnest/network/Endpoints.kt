d6ad3349bc2b2489c9ed33f79f149e4937fa612f
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2022-12-04 23:21:44 +0300

    [Android] Changed base url

--- data/src/commonMain/kotlin/com/fitnest/network/Endpoints.kt
+++ data/src/commonMain/kotlin/com/fitnest/network/Endpoints.kt
@@ -1,7 +1,7 @@
 package com.fitnest.network
 
 object Endpoints {
-    const val BASE_URL = "https://fitnestappgo.herokuapp.com/"
+    const val BASE_URL = "https://fitnestapp.onrender.com/"
 
     object Flow : Group {
         override val name = "flow"
