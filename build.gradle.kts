a8a6226aab303ef0ee38f9f4f301350021499b5c
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2023-02-03 23:51:27 +0300

    [Android] Downgraded kotlin version to 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.android.tools.build:gradle:7.4.1")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
