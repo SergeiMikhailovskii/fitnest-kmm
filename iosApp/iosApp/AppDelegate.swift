f1303b189419e9f091721a11a56e4835bcfc273c
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2023-11-05 12:36:00 +0300

    [iOS] Napier setup

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2023 orgName. All rights reserved.
 //
 
+import presentation
 import SwiftUI
 
 class AppDelegate : NSObject, UIApplicationDelegate {
     let rootHolder = RootHolder()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        NapierKt.doInitNapier()
+        return true
+    }
 }
